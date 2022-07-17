df9d8cd16c7d818c9ec8776a4867053757b1f849
Ian Renyard <ian@renyard.net>
2022-07-17 15:16:21 +0100

    Renamed js files to jsx and added Button, Keypad and Screen

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Screen from "./components/Screen";
+import Keypad from "./components/Keypad";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Screen />
+      <Keypad />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// React button component
+const Button = (props) => {
+  const { children } = props;
+  return <div>{children}</div>;
+};
+
+export default Button;
